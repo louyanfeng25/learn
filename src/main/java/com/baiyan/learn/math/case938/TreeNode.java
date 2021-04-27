196a999d8b890d06ae6c461738165124b39107e4
baiyan <lyf13094820660@qq.com>
2021-04-27 11:19:05 +0800

    feat : leetCode 938

---
+++ src/main/java/com/baiyan/learn/math/case938/TreeNode.java
@@ -0,0 +1,18 @@
+package com.baiyan.learn.math.case938;
+
+/**
+ * @author baiyan
+ * @time 2021/4/27 10:18
+ */
+public class TreeNode {
+  int val;
+  TreeNode left;
+  TreeNode right;
+  TreeNode() {}
+  TreeNode(int val) { this.val = val; }
+  TreeNode(int val, TreeNode left, TreeNode right) {
+      this.val = val;
+      this.left = left;
+      this.right = right;
+  }
+}
