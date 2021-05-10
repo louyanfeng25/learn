ee11791984a16e2e30643205c01cf67242bc8092
baiyan <lyf13094820660@qq.com>
2021-05-10 21:58:45 +0800

    feat : 链表倒数第K个节点代码提交

---
+++ src/main/java/com/baiyan/learn/math/caseOffer22/ListNode.java
@@ -0,0 +1,16 @@
+package com.baiyan.learn.math.caseOffer22;
+
+/**
+ * @author baiyan
+ * @time 2021/5/10 21:10
+ */
+public class ListNode {
+
+    int val;
+    ListNode next = null;
+
+    ListNode(int val) {
+        this.val = val;
+    }
+
+}
