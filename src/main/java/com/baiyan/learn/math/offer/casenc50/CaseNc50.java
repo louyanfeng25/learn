1ebc5583f9558770a1454403a1ec34db9ed68045
baiyan <lyf13094820660@qq.com>
2021-11-01 17:13:21 +0800

    feat : NC50 链表中的节点每k个一组翻转

---
+++ src/main/java/com/baiyan/learn/math/offer/casenc50/CaseNc50.java
@@ -0,0 +1,51 @@
+package com.baiyan.learn.math.offer.casenc50;
+
+
+/**
+ * NC50 链表中的节点每k个一组翻转
+ *
+ * @author baiyan
+ * @date 2021/11/01
+ */
+public class CaseNc50 {
+
+    public ListNode reverseKGroup(ListNode head, int k) {
+        if(head == null || head.next == null || k < 2){
+            return head;
+        }
+        ListNode dummy = new ListNode(0);
+        dummy.next = head;
+        ListNode pre = dummy, cur = head, temp;
+        int len = 0;
+        while (head != null) {
+            len ++ ;
+            head = head.next;
+        }
+        for (int i = 0; i < len/k; i++) {
+            for (int j = 1; j < k; j ++ ) {
+                temp = cur.next;
+                cur.next = temp.next;
+                temp.next = cur;
+                pre.next = temp;
+            }
+            pre = cur;
+            cur = cur.next;
+        }
+        return dummy.next;
+    }
+
+    public static void main(String[] args) {
+        ListNode a = new ListNode(1);
+        ListNode b = new ListNode(2);
+        ListNode c = new ListNode(3);
+        ListNode d = new ListNode(4);
+        ListNode e = new ListNode(5);
+        a.next = b;
+        b.next = c;
+        c.next = d;
+        d.next = e;
+        CaseNc50 caseNc50 = new CaseNc50();
+        ListNode listNode = caseNc50.reverseKGroup(a, 2);
+        System.out.println(listNode);
+    }
+}
