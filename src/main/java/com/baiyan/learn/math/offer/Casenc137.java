dc172a9e2efd1770018acf122606884e4006743f
baiyan <lyf13094820660@qq.com>
2021-11-19 10:48:13 +0800

    feat : 自己再打一遍逻辑，还是有点难理解

--- src/main/java/com/baiyan/learn/math/offer/Casenc137.java
+++ src/main/java/com/baiyan/learn/math/offer/Casenc137.java
@@ -8,7 +8,7 @@ import java.util.Stack;
  * NC137 表达式求值
  *
  * <href>
- *     https://www.nowcoder.com/practice/c215ba61c8b1443b996351df929dc4d4?tpId=117&&tqId=37849&rp=1&ru=/activity/oj&qru=/ta/job-code-high/question-ranking
+ * https://www.nowcoder.com/practice/c215ba61c8b1443b996351df929dc4d4?tpId=117&&tqId=37849&rp=1&ru=/activity/oj&qru=/ta/job-code-high/question-ranking
  * </href>
  *
  * @author baiyan
@@ -16,93 +16,93 @@ import java.util.Stack;
  */
 public class Casenc137 {
 
-    public int solve (String s) {
-        Stack<Character> op_stack = new Stack<Character>();
-        Stack<Integer> num_stack = new Stack<Integer>();
-        Map<Character, Integer> map = new HashMap<Character, Integer>(){{
-            put('(', 0);
-            put('+', 1);
-            put('-', 1);
-            put('*', 2);
-        }};
-
-        int ans = 0;
-
-        for(int i = 0; i < s.length(); i++){
-            //如果是(，就加入操作符栈
-            if(s.charAt(i) == '('){
-                op_stack.push(s.charAt(i));
-            }
-            //如果是)，就计算直到遇到(
-            else if (s.charAt(i) == ')'){
-                while(!op_stack.isEmpty()){
-                    if(op_stack.peek() != '('){
-                        cal(op_stack, num_stack);
-                    } else{
+    public int solve(String s) {
+        Stack<Integer> num_stack = new Stack<>();
+        Stack<Character> op_stack = new Stack<>();
+        HashMap<Character, Integer> map = new HashMap<>();
+        map.put('(', 0);
+        map.put('+', 1);
+        map.put('-', 1);
+        map.put('*', 2);
+        char[] chars = s.toCharArray();
+        for (int i = 0; i < chars.length; i++) {
+            // '('直接入栈
+            if (chars[i] == '(') {
+                op_stack.push(chars[i]);
+                // '）'遇到）则需要遍历操作数栈，如果操作数栈不为空，在遇到'（'前将结果计算下来缓存
+                // 由于在最后一个else入栈操作符时，每次均会将前置的逻辑合并，因此只会至多留下两个数字跟一个操作符
+            } else if (chars[i] == ')') {
+                //需要）后，需要将（）中间的所有结果结算完结
+                while (!op_stack.isEmpty()) {
+                    if (op_stack.peek() != '(') {
+                        cal(num_stack, op_stack);
+                    } else {
                         op_stack.pop();
                         break;
                     }
                 }
-            }
-            else{
-                //如果是数字，就压入操作数栈
-                if(Character.isDigit(s.charAt(i))){
-                    int j = i;
-                    while(j < s.length() && Character.isDigit(s.charAt(j))){
-                        j++;
-                    }
-                    //多位需要截取字符串
-                    num_stack.push(Integer.valueOf(s.substring(i, j)));
-                    i = j - 1;
+                //数字入栈需要截取出大于10的数字
+            } else if (Character.isDigit(chars[i])) {
+                int j = i;
+                while (j < chars.length && Character.isDigit(chars[j])) {
+                    j++;
                 }
-                //如果是+-*操作符，则将其在(前的，运算优先级高的运算都做完
-                //然后再压入当前操作符
-                else{
-                    while(!op_stack.isEmpty() && op_stack.peek() != '('){
-                        if(map.get(s.charAt(i)) <= map.get(op_stack.peek())){
-                            cal(op_stack, num_stack);
-                        }
-                        else{
-                            break;
-                        }
-
+                Integer integer = Integer.valueOf(s.substring(i, j));
+                num_stack.push(integer);
+                i = j - 1;
+                //循环计算（之前的数据，再将操作符入栈，这一步很重要，
+                //操作符+-*入栈之前，需要将栈中（之前的符号数据按照优先级先进行合并计算
+            } else {
+                while (!op_stack.isEmpty() && op_stack.peek() != '(') {
+                    if (map.get(chars[i]) <= map.get(op_stack.peek())) {
+                        cal(num_stack, op_stack);
+                    } else {
+                        break;
                     }
-                    op_stack.push(s.charAt(i));
                 }
+                op_stack.push(chars[i]);
             }
         }
-
-        //已经将所有带括号的都计算完成，将中间剩余的结果计算完成计算完
-        while(!op_stack.isEmpty()){
-            cal(op_stack, num_stack);
+        //还会剩下去除括号后的数据
+        while (!op_stack.isEmpty()) {
+            cal(num_stack, op_stack);
         }
         return num_stack.peek();
     }
 
     /**
-     * 根据运算符进行计算，注意栈中先拿出来的数字是在运算符之后的数字
+     * 计算结果数据，将计算后的结果数据再次压栈等待后续关联计算
      *
-     * @param op
-     * @param num
+     * @param num_stack
+     * @param op_stack
      */
-    public void cal (Stack<Character> op, Stack<Integer> num){
-        if(op.isEmpty() || num.isEmpty() || num.size() < 2){
+    public void cal(Stack<Integer> num_stack, Stack<Character> op_stack) {
+        //防止直接出现+3，或者3这种情况
+        if (num_stack.isEmpty() || op_stack.isEmpty() || num_stack.size() < 2) {
             return;
         }
-        int b = num.pop();
-        int a = num.pop();
-        switch (op.pop()){
+        int num2 = num_stack.pop();
+        int num1 = num_stack.pop();
+        switch (op_stack.pop()) {
             case '+':
-                num.push(a + b);
+                num_stack.push(num1 + num2);
                 break;
             case '-':
-                num.push(a - b);
+                num_stack.push(num1 - num2);
                 break;
             case '*':
-                num.push(a * b);
+                num_stack.push(num1 * num2);
                 break;
             default:
                 break;
         }
+    }
+
+
+    public static void main(String[] args) {
+        Casenc137 casenc137 = new Casenc137();
+        //50
+        int solve = casenc137.solve("3*(1+2*(3+4))+5");
+        System.out.println(solve);
     }
 }
