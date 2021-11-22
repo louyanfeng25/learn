4f9c594cee2cde6c8d3e9a7b89baece5ec3d5132
baiyan <lyf13094820660@qq.com>
2021-11-22 13:43:38 +0800

    feat : NC96 判断一个链表是否为回文结构

---
+++ src/main/java/com/baiyan/learn/math/offer/casenc96/ListNode.java
@@ -0,0 +1,11 @@
+package com.baiyan.learn.math.offer.casenc96;
+
+/**
+ * @author baiyan
+ * @time 2021/6/7 17:21
+ */
+public class ListNode {
+    int val;
+    ListNode next;
+    ListNode(int x) { val = x; }
+}
