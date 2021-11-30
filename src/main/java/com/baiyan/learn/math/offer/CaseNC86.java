a22a659a7a31c33d358d26730c6af4bc10d540af
baiyan <lyf13094820660@qq.com>
2021-11-30 18:34:55 +0800

    feat : NC86 矩阵元素查找

---
+++ src/main/java/com/baiyan/learn/math/offer/CaseNC86.java
@@ -0,0 +1,33 @@
+package com.baiyan.learn.math.offer;
+
+/**
+ * NC86 矩阵元素查找
+ *
+ * <href>
+ *     https://www.nowcoder.com/practice/3afe6fabdb2c46ed98f06cfd9a20f2ce?tpId=117&&tqId=37788&rp=1&ru=/activity/oj&qru=/ta/job-code-high/question-ranking
+ * </href>
+ *
+ * @author baiyan
+ * @time 2021/11/30
+ */
+public class CaseNC86 {
+
+    public int[] findElement(int[][] mat, int n, int m, int x) {
+        int maxN = n;
+        n = 0;
+        m = m-1;
+        while (n<maxN && m>=0){
+            if(mat[n][m] == x){
+                return new int[]{n,m};
+            }
+            if(mat[n][m] > x){
+                m--;
+            }
+            if(mat[n][m] < x){
+                n++;
+            }
+        }
+        return new int[]{};
+    }
+
+}
