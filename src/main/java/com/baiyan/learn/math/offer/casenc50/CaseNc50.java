4ef74441b47fea2c1f350e5d347cf68f2b23e238
baiyan <lyf13094820660@qq.com>
2022-02-11 16:49:38 +0800

    feat : 复习NC50 链表中的节点每k个一组翻转

--- src/main/java/com/baiyan/learn/math/offer/casenc50/CaseNc50.java
+++ src/main/java/com/baiyan/learn/math/offer/casenc50/CaseNc50.java
@@ -5,24 +5,31 @@ package com.baiyan.learn.math.offer.casenc50;
  * NC50 链表中的节点每k个一组翻转
  *
  * @author baiyan
- * @date 2021/11/01
+ * @date 2022/02/11
  */
 public class CaseNc50 {
 
     public ListNode reverseKGroup(ListNode head, int k) {
-        if(head == null || head.next == null || k < 2){
+        if(head ==null || head.next==null || k<2){
             return head;
         }
-        ListNode dummy = new ListNode(0);
+
+        ListNode dummy = new ListNode(-1);
         dummy.next = head;
-        ListNode pre = dummy, cur = head, temp;
+
+        ListNode cur = head;
+        ListNode pre = dummy;
+        ListNode temp;
+
         int len = 0;
-        while (head != null) {
-            len ++ ;
+        while (head!=null){
+            len++;
             head = head.next;
         }
+
         for (int i = 0; i < len/k; i++) {
-            for (int j = 1; j < k; j ++ ) {
+            //头插法
+            for (int j = 1; j < k; j++) {
                 temp = cur.next;
                 cur.next = temp.next;
                 temp.next = pre.next;
