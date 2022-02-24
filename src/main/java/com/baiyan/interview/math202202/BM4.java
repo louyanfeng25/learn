e89272209a03f96d3e04ce8f6819f1f0d1168260
baiyan <lyf13094820660@qq.com>
2022-02-24 10:53:10 +0800

    feat : BM4 合并两个排序的链表

---
+++ src/main/java/com/baiyan/interview/math202202/BM4.java
@@ -0,0 +1,33 @@
+package com.baiyan.interview.math202202;
+
+/**
+ * BM4 合并两个排序的链表
+ *
+ * @author baiyan
+ * @date 2022-02-24
+ */
+public class BM4 {
+
+    public ListNode Merge(ListNode l1,ListNode l2) {
+        ListNode pre = new ListNode(-1);
+        ListNode dummy = pre;
+        while (l1!=null && l2!=null){
+            if(l1.val<=l2.val){
+                dummy.next = l1;
+                l1 = l1.next;
+            }else {
+                dummy.next = l2;
+                l2 = l2.next;
+            }
+            dummy = dummy.next;
+        }
+        if(l1!=null){
+            dummy.next = l1;
+        }
+        if(l2!=null){
+            dummy.next = l2;
+        }
+        return pre.next;
+    }
+
+}
