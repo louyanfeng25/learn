f6338d90e814cca13231574c2a4db009de31963e
baiyan <lyf13094820660@qq.com>
2022-02-24 16:47:32 +0800

    feat : BM7 链表中环的入口结点

---
+++ src/main/java/com/baiyan/interview/math202202/BM7.java
@@ -0,0 +1,25 @@
+package com.baiyan.interview.math202202;
+
+import java.util.HashMap;
+
+/**
+ * BM7 链表中环的入口结点
+ *
+ * @author baiyan
+ * @date 2022-02-24
+ */
+public class BM7 {
+
+    public ListNode EntryNodeOfLoop(ListNode pHead) {
+        HashMap<ListNode, Integer> map = new HashMap<>(8);
+        while (pHead!=null){
+            Integer exist = map.get(pHead);
+            if(exist!=null){
+                return pHead;
+            }
+            map.put(pHead,1);
+            pHead = pHead.next;
+        }
+        return null;
+    }
+}
