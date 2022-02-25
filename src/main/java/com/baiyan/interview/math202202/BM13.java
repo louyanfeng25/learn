88c7074909c99427cdf8ead4657d1132ac35e7f7
baiyan <lyf13094820660@qq.com>
2022-02-25 10:45:09 +0800

    feat : BM13 判断一个链表是否为回文结构

---
+++ src/main/java/com/baiyan/interview/math202202/BM13.java
@@ -0,0 +1,29 @@
+package com.baiyan.interview.math202202;
+
+import java.util.Stack;
+
+/**
+ * BM13 判断一个链表是否为回文结构
+ *
+ * @author baiyan
+ * @date 2022-02-25
+ */
+public class BM13 {
+
+    public boolean isPail (ListNode head) {
+        ListNode start = head;
+        Stack<ListNode> stack = new Stack<>();
+        while (start!=null){
+            stack.add(start);
+            start = start.next;
+        }
+        while (head!=null){
+            ListNode pop = stack.pop();
+            if(head.val!=pop.val){
+                return false;
+            }
+            head = head.next;
+        }
+        return true;
+    }
+}
