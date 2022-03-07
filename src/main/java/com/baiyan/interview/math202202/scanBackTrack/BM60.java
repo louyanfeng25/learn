4c894050384d7650663c86f1bb1e136b5bff5621
baiyan <lyf13094820660@qq.com>
2022-03-07 14:31:07 +0800

    feat : BM60 括号生成

--- src/main/java/com/baiyan/interview/math202202/scanBackTrack/BM60.java
+++ src/main/java/com/baiyan/interview/math202202/scanBackTrack/BM60.java
@@ -1,20 +1,16 @@
-package com.baiyan.learn.math.offer;
+package com.baiyan.interview.math202202.scanBackTrack;
 
 import java.util.ArrayList;
 
 /**
- * 26.括号生成
- *
- * <href>
- *     https://www.nowcoder.com/practice/c9addb265cdf4cdd92c092c655d164ca?tpId=117&&tqId=37748&rp=1&ru=/activity/oj&qru=/ta/job-code-high/question-ranking
- * </href>
+ * BM60 括号生成
  *
  * @author baiyan
- * @time 2021/12/06
+ * @date 2022-03-07
  */
-public class CaseNC26 {
+public class BM60 {
 
-    ArrayList<String>  result = new ArrayList<>();
+    ArrayList<String> result = new ArrayList<>();
 
     public ArrayList<String> generateParenthesis (int n) {
         if(n<=0){
@@ -38,22 +34,15 @@ public class CaseNC26 {
             result.add(str);
             return;
         }
-        //左右括号相等，则下一个括号必须使用左括号，保证左括号小于等于右括号
-        if(left==right){
+        //左右括号相等，只能添加左括号
+        if(right==left){
             generateParenthesis(str+"(",left-1,right);
         }else {
-            //如果左括号小于右括号的情况下，则下一个左右括号都可以，
             if(left>0){
                 generateParenthesis(str+"(",left-1,right);
             }
             generateParenthesis(str+")",left,right-1);
         }
-    }
-
-    public static void main(String[] args) {
-        CaseNC26 caseNC26 = new CaseNC26();
-        ArrayList<String> strings = caseNC26.generateParenthesis(3);
-        strings.forEach(e-> System.out.println(e));
     }
 
 }
